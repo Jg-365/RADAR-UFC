d407651183f9e77ce4b65f853d03b20586888f8d
Jg-365 <jguilherme19@alu.ufc.br>
2026-06-25 18:06:23 -0300

    feat: add repository contract

---
+++ src/js/data/Repository.js
@@ -0,0 +1,13 @@
+export class Repository {
+  async list() {
+    throw new Error("Repository.list not implemented");
+  }
+
+  async getById() {
+    throw new Error("Repository.getById not implemented");
+  }
+
+  async save() {
+    throw new Error("Repository.save not implemented");
+  }
+}
